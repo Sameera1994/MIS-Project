491eb0272238338f846c68264e8bb259e0adb83f
Sameera Lakshitha <lakshithasameera89@gmail.com>
2024-12-09 00:47:31 +0530

    Update footer.php

--- app/Views/footer/footer.php
+++ app/Views/footer/footer.php
@@ -28,13 +28,6 @@
     <div class="footer-bg">
         <div class="container">
             <footer class="py-3 mt-4">
-                <ul class="nav justify-content-center border-bottom pb-3 mb-3 border-white ">
-                    <li class="nav-item"><a href="#" class="nav-link px-2 footer-items">Home</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link px-2 footer-items">Features</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link px-2 footer-items">Pricing</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link px-2 footer-items">FAQs</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link px-2 footer-items">About</a></li>
-                </ul>
                 <p class="text-center footer-items">&copy; 2024 Sabaragamuwa University of Sri Lanka</p>
             </footer>
         </div>
